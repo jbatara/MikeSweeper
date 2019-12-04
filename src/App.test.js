ba07c6ed54f0c3d17e49738292a5da61718edb37
Jen and Mike <mmcshane10@gmail.com>
2019-12-04 11:23:47 -0800

    Add test for calculateNumbers

--- src/App.test.js
+++ src/App.test.js
@@ -31,3 +31,28 @@ it('returns a list of the adjacent squares',() =>{
   const squares=[[0,1],[1,0],[1,1]];
   expect(app.adjacentSquares(coord)).toEqual(squares);
 });
+
+it('returns a gameBoard with number of mines around that square',() =>{
+  const app = new App();
+  
+  //create empty game board
+  let gameBoard = app.createGameBoard(10);
+
+  //make bomb coordinate at origin
+  let mineCoordinates = [[0,0]];
+
+  //copy game board for board with just the mine
+  let gameBoardWithMine = [...gameBoard];
+  //add mine to copied game board
+  app.addMines(gameBoardWithMine, mineCoordinates);
+
+  //make 2nd copy of game board that is used as the expected value of test
+  let expectedGameBoard = [...gameBoard];
+  //add expected values in the 2nd copy of game board
+  expectedGameBoard[1][0] = "1";
+  expectedGameBoard[1][1] = "1";
+  expectedGameBoard[1][0] = "1";
+
+
+  expect(app.calculateNumbers(mineCoordinates,gameBoardWithMine)).toEqual(expectedGameBoard);
+});
