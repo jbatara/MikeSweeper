4be3143f4b7c4c2c30ffc1e1ede3bc45519d0e10
Jen and Mike <mmcshane10@gmail.com>
2019-12-04 14:06:46 -0800

    Make functions use variables instead of magic numbers

--- src/components/App.js
+++ src/components/App.js
@@ -5,25 +5,23 @@ import { directive } from '@babel/types';
 import { Switch, Route } from 'react-router-dom';
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.createGameBoard = this.createGameBoard.bind(this);
     this.generateMines = this.generateMines.bind(this);
     this.adjacentSquares = this.adjacentSquares.bind(this);
-    this.state ={
+    this.state = {
       size: 10,
       gameBoard: this.createGameBoard(10),
       mineCoords: this.generateMines(1)
     }
   }
 
-  createGameBoard(size){
+  createGameBoard(size) {
     let gameBoard = [];
-    for(let i =0; i < size; i++)
-    {
-      let row=[];
-      for(let j = 0; j<size; j++)
-      {
+    for (let i = 0; i < size; i++) {
+      let row = [];
+      for (let j = 0; j < size; j++) {
         row.push('0');
       }
       gameBoard.push(row);
@@ -31,31 +29,27 @@ class App extends React.Component {
     return gameBoard;
   }
 
-  generateMines(quantity){
+  generateMines(quantity) {
     const gameBoardSize = 10;
     let mineCoords = [];
-    while(mineCoords.length < quantity)
-    {
-      let x = Math.floor(Math.random()*gameBoardSize);
-      let y = Math.floor(Math.random()*gameBoardSize);
-      let newCoord = [x,y];
-      if(!mineCoords.includes(newCoord)){
+    while (mineCoords.length < quantity) {
+      let x = Math.floor(Math.random() * gameBoardSize);
+      let y = Math.floor(Math.random() * gameBoardSize);
+      let newCoord = [x, y];
+      if (!mineCoords.includes(newCoord)) {
         mineCoords.push(newCoord);
       }
     }
     return mineCoords;
   }
 
-  addMines(gameBoard, mineCoords){
+  addMines(gameBoard, mineCoords) {
     const gameBoardSize = gameBoard.length;
     // const mineCoords = this.state.mineCoords;
-    for(let row = 0; row < gameBoardSize; row++)
-    {
-      for(let column = 0; column < gameBoardSize; column++)
-      {
+    for (let row = 0; row < gameBoardSize; row++) {
+      for (let column = 0; column < gameBoardSize; column++) {
         const currentCoord = [row, column];
-        if(mineCoords.includes(currentCoord))
-        {
+        if (mineCoords.includes(currentCoord)) {
           gameBoard[row][colunn] = "mine";
         }
       }
@@ -63,29 +57,29 @@ class App extends React.Component {
     return gameBoard;
   }
 
-  coordinateEquality(coordinate1, coordinate2){
-    if(coordinate1[0] === coordinate2[0] && coordinate1[1] === coordinate2[1]){
+  coordinateEquality(coordinate1, coordinate2) {
+    if (coordinate1[0] === coordinate2[0] && coordinate1[1] === coordinate2[1]) {
       return true;
     }
-    else{
+    else {
       return false;
     }
   }
 
-  adjacentSquares(coordinates){ //this function will return all valid adjacent squares to the given coordinates
+  adjacentSquares(coordinates) { //this function will return all valid adjacent squares to the given coordinates
     let adjacentSquares = [];
     let gameBoardSize = 10;
     const originalX = coordinates[0];
     const originalY = coordinates[1];
-    const newOriginX = originalX -1;
-    const newOriginY = originalY -1;
+    const newOriginX = originalX - 1;
+    const newOriginY = originalY - 1;
 
-    for(let row=0; row <3; row++){
+    for (let row = 0; row < 3; row++) {
       let squareX = newOriginX + row;
-      for(let column=0; column<3; column++){
-        let squareY = newOriginY+column;
+      for (let column = 0; column < 3; column++) {
+        let squareY = newOriginY + column;
         let square = [squareX, squareY];
-        if(!this.coordinateEquality([originalX, originalY], square) && squareY >= 0 && squareX >= 0 && squareY <gameBoardSize && squareX <gameBoardSize){
+        if (!this.coordinateEquality([originalX, originalY], square) && squareY >= 0 && squareX >= 0 && squareY < gameBoardSize && squareX < gameBoardSize) {
           adjacentSquares.push(square);
         }
       }
@@ -93,18 +87,17 @@ class App extends React.Component {
     return adjacentSquares;
   }
 
-  calculateNumbers(mineCoords, gameBoard){
+  calculateNumbers(mineCoords, gameBoard) {
     const gameBoardLength = this.state.gameBoard.length;
-    for(let i=0; i < gameBoardLength; i++){
-      for(let j=0; j < gameBoardLength; j++){
-        const currentCoord = [i,j];
+    for (let i = 0; i < gameBoardLength; i++) {
+      for (let j = 0; j < gameBoardLength; j++) {
+        const currentCoord = [i, j];
         const content = gameBoard[i][j];
-        if(content !== "mine"){
-          const adjacentSquares = this.adjacentSquares([i,j]);
+        if (content !== "mine") {
+          const adjacentSquares = this.adjacentSquares([i, j]);
           let counter = 0;
-          adjacentSquares.forEach(function(squareCoord){
-            if(mineCoords.includes(squareCoord))
-            {
+          adjacentSquares.forEach(function (squareCoord) {
+            if (mineCoords.includes(squareCoord)) {
               counter++
             }
           });
@@ -115,12 +108,19 @@ class App extends React.Component {
     return gameBoard;
   }
 
+  componentToRender(component) {
+    switch (component) {
+      case 'start':
+        return <PlayArea />;
+      default:
+        return <Menu />;
+    }
+  }
+
   render() {
     return (
       <div>
-        <Switch>
-          
-        </Switch>
+        {this.componentToRender(this.state.display)}
       </div>
     );
   }
