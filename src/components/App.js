fa0f51ac264ca56beefb05377de0ef6f84486422
Jen and Mike <mmcshane10@gmail.com>
2019-12-04 10:33:42 -0800

    Add filter into adjacent squares

--- src/components/App.js
+++ src/components/App.js
@@ -8,8 +8,10 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.createGameBoard = this.createGameBoard.bind(this);
+    this.generateMines = this.generateMines.bind(this);
     this.state ={
-      gameBoard: this.emptyGameBoard(10),
+      size: 10,
+      gameBoard: this.createGameBoard(10),
       mineCoords: this.generateMines(1)
     }
   }
@@ -29,7 +31,7 @@ class App extends React.Component {
   }
 
   generateMines(quantity){
-    
+    const gameBoardSize = 10;
     let mineCoords = [];
     while(mineCoords.length < quantity)
     {
@@ -58,6 +60,35 @@ class App extends React.Component {
       }
     }
     return gameBoard;
+  }
+
+  coordinateEquality(coordinate1, coordinate2){
+    if(coordinate1[0] === coordinate2[0] && coordinate1[1] === coordinate2[1]){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  adjacentSquares(coordinates){ //this function will return all valid adjacent squares to the given coordinates
+    let adjacentSquares = [];
+    const originalX = coordinates[0];
+    const originalY = coordinates[1];
+    const newOriginX = originalX -1;
+    const newOriginY = originalY -1;
+
+    for(let row=0; row <3; row++){
+      let squareX = newOriginX + row;
+      for(let column=0; column<3; column++){
+        let squareY = newOriginY+column;
+        let square = [squareX, squareY];
+        if(!this.coordinateEquality([originalX, originalY], square) && squareY >= 0 && squareX >= 0 && squareY <10 && squareX <10){
+          adjacentSquares.push(square);
+        }
+      }
+    }
+    return adjacentSquares;
   }
 
   render() {
